d5b42bf6ceab1811df8249b1f48e42206884df02
Faisal Mohammad <m.faisal.92.27@gmail.com>
2016-05-25 11:03:07 -0700

    Final

--- app.js
+++ app.js
@@ -188,10 +188,10 @@ var url = "mongodb://52.36.216.12:27017,52.11.239.130:27017,52.37.112.11:27017/t
 
 
     MongoClient.connect(url, function(err, db) {
-        var cursor =db.collection('mycol').find();
-        var doc_total = {};
+       // var cursor =db.collection('mycol').find();
+        var doc_total = {"result":"Hello World"};
         var index = 0;
-	res.end(JSON.stringify({"result":"Hello World"});//(doc_total),null,4));
+	res.end(JSON.stringify(doc_total),null,4));
 /*
         cursor.each(function(err, doc) {
           console.log("Doc is "+doc)
